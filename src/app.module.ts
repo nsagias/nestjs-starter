a8f7a81b17bf7a5b7499a79f81f105659d6deb02
Nick <nsagias@gmail.com>
2023-05-18 23:58:24 -0400

    create starter module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StaterModule } from './stater/stater.module';
+import { StarterModule } from './starter/starter.module';
 
 @Module({
-  imports: [],
+  imports: [StaterModule, StarterModule],
   controllers: [AppController],
   providers: [AppService],
 })
