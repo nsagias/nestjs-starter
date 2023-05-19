1be8b1133f0916044749cf8d738f62c2c050fe25
Nick <nsagias@gmail.com>
2023-05-19 19:14:12 -0400

    add query example

--- src/starter/starter.controller.ts
+++ src/starter/starter.controller.ts
@@ -1,16 +1,26 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller('starter')
 export class StarterController {
-  // GET /users  --> []
-  @Get()
-  getStarters() {
-    return [
-      { userId: "1"},{ userId: "2"}
-    ];
-  }
+
+  // GET /starter/:id --> { id: }
   @Get(":id")
   getStarter(@Param("id") id: string) {
     return { id };
+  }
+
+  // Get query /starter?type=<what to query>
+  @Get()
+  getStarterQuery(@Query("type") type: string) {
+    return [{ type }];
+  }
+
+  // GET /starter --> []
+  @Get()
+  getStarters() {
+    return [
+      { userId: "1"},
+      { userId: "2"}
+    ];
   }
 }
