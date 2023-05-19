951f7bde709ab004037894ccae4783187569f7c1
Nick <nsagias@gmail.com>
2023-05-19 01:25:35 -0400

    create starter path

--- src/starter/starter.controller.ts
+++ src/starter/starter.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('starter')
-export class StarterController {}
+export class StarterController {
+  // GET /users  --> []
+  @Get()
+  getStarters() {
+    return [
+      { userId: "1"},{ userId: "2"}
+    ];
+  }
+}
