7b13a174b379326a71291d6705a6dbdd2a93bfc9
Nick <nsagias@gmail.com>
2023-05-19 00:10:45 -0400

    create user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StarterModule } from './starter/starter.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [StarterModule],
+  imports: [StarterModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
